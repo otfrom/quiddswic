4d920837f80f13b3594d7a0f2cca241269e1f596
Bruce Durling <bld@otfrom.com>
2012-05-20 18:10:17 +0100

    Better charts and more analysis.

--- src/quiddswic/core.clj
+++ src/quiddswic/core.clj
@@ -46,13 +46,87 @@
 
 (def sm-ord (incanter/$order :season :asc sm))
 
-(def s3 (incanter/$join [:season :dow] sm s2))
+(def sm-sd
+  (let [sd1 (stats/sd (incanter/$ :beer s3))
+        sd1-high (incanter/$map (fn [m] (+ sd1 m)) [:mean] sm-ord)
+        sd1-low (incanter/$map (fn [m] (- m sd1)) [:mean] sm-ord)
+        sd2-high (incanter/$map (fn [m] (+ (* 2 sd1) m)) [:mean] sm-ord)
+        sd2-low (incanter/$map (fn [m] (- m (* 2 sd1))) [:mean] sm-ord)]
+    (-> (incanter/conj-cols sm-ord sd1-high sd1-low sd2-high sd2-low)
+        (incanter/col-names [:season :mean :sd1-high :sd1-low :sd2-high :sd2-low]))))
+
+(def s3 (incanter/$join [:season :dow] sm-sd s2))
+
+(def s-alerts
+  (-> (incanter/$map
+       (fn [m high low]
+         (cond (> m high) 1
+               (< m low) -1
+               :else 0))
+       [:beer :sd1-high :sd1-low] s3)
+      (incanter/col-names [:alert])
+      (incanter/conj-cols s3)))
 
 (incanter/head s3)
 
 (stats/mean (incanter/$ :beer s3))
 
-(def ts-plot
-  (charts/time-series-plot (incanter/$ :millis s1) (incanter/$ :beer s1)))
+(def april-2011
+  (incanter/$where
+   {:millis
+    {:$gte (coerce/to-long (time/date-time 2011 04 01))
+     :$lte (coerce/to-long (time/date-time 2011 04 30))}}
+   s-alerts))
 
-(incanter/view ts-plot)
+(defn ts-plot [ts theme]
+  (doto
+      (charts/time-series-plot
+       (incanter/$ :millis ts)
+       (incanter/$ :beer ts)
+       :x-label "Date"
+       :y-label "Pints of Beer"
+       :legend true
+       :series-label "Pints over time")
+    (charts/set-theme theme)))
+
+(incanter/view (ts-plot s-alerts :dark))
+
+(incanter/view (ts-plot april-2011 :dark))
+
+(defn plot-alert-chart [ts theme]
+  (doto
+      (charts/time-series-plot
+       :millis :beer
+       :data ts
+       :x-label "Date" :y-label "Pints of Beer"
+       :series-label "Pints over time"
+       :legend true)
+    (charts/set-theme theme)
+    (charts/add-lines
+     (incanter/$ [:millis] ts)
+     (incanter/$ [:sd1-low] ts)
+     :series-label "Low Threshold")
+    (charts/add-lines
+     (incanter/$ [:millis] ts)
+     (incanter/$ [:sd1-high] ts)
+     :series-label "High Threshold")
+    (charts/add-points
+     :millis :beer :data
+     (incanter/$where {:alert -1} ts)
+     :series-label "Low beer alert!")
+    ;; Main Metric
+    (charts/set-stroke :dataset 0 :width 5)
+    (charts/set-stroke-color java.awt.Color/yellow :dataset 0)
+    ;; High Threshold
+    (charts/set-stroke :dataset 1 :width 2 :dash 5)
+    (charts/set-stroke-color java.awt.Color/green :dataset 1)
+    ;; Low Threshold
+    (charts/set-stroke :dataset 2 :width 2 :dash 5)
+    (charts/set-stroke-color java.awt.Color/red :dataset 2)
+    ;; Low Beer Alerts
+    (charts/set-stroke :dataset 3 :width 10)
+    (charts/set-stroke-color java.awt.Color/red :dataset 3)))
+
+(incanter/view (plot-alert-chart s-alerts :dark))
+
+(incanter/view (plot-alert-chart april-2011 :dark))
